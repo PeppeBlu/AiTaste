658fa69a499130eb80f11c17f96d2383df2a8f03
Peppe <giuseppe.blunda@edu.unife.it>
2025-04-07 23:39:09 +0200

    removed sensible data

--- aitaste.py
+++ aitaste.py
@@ -1,7 +1,11 @@
 import openai
 import gradio as gr
+from dotenv import load_dotenv
+import os
 
 # Configura OpenRouter con la tua API key
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_base = "https://openrouter.ai/api/v1"
 
